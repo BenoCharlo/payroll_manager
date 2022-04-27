58652d45b93ea560bedf212cd325aea9c19f3462
Charles <dokodjokwassibenocharles@yahoo.com>
2022-04-28 00:33:08 +0200

    update header with children component

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,12 +1,13 @@
 import styles from './header.module.css'
-
-
 interface HeaderProps {
     children: React.ReactNode;
  }
 
 export default function Header({children}: HeaderProps) {
   return (
-    <div className={styles.container}>My header</div>
+    <div className={styles.container}>
+      My header
+      {children}
+    </div>
   )
 }
