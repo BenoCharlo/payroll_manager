9915cf7989fe1879e7843abf5887dba29b2b9d2b
Charles <dokodjokwassibenocharles@yahoo.com>
2022-04-28 17:53:54 +0200

    add navar icons

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,12 +1,24 @@
 import styles from "../styles/Navbar.module.css"
+import {IoIosLogOut} from "react-icons/io"
+import {BiUser} from "react-icons/bi"
+import {MdOutlineAvTimer} from "react-icons/md"
 
 export const Navbar = () => {
   return (
     <div className={styles.nav}>
       <ul className={styles.nav__menu}>
-        <li className={styles.menu__item}>Leave Balance</li>
-        <li className={styles.menu__item}>Username</li>
-        <li className={styles.menu__item}>Sign Out</li>
+        <li className={styles.menu__item}>
+        <span className={styles.icon}><MdOutlineAvTimer /></span>
+          Leave Balance
+          </li>
+        <li className={styles.menu__item}>
+        <span className={styles.icon}><BiUser /></span>
+          Username
+          </li>
+        <li className={styles.menu__item}>
+        <span className={styles.icon}><IoIosLogOut /></span>
+          Sign Out
+          </li>
       </ul>
     </div>
   )
